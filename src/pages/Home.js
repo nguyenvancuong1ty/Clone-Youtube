e1bf5d9cd39652fe705b964c4dc98d3fe85231ae
Cuong Nguyen 2k1 <113027848+nguyenvancuong1ty@users.noreply.github.com>
2026-03-26 14:45:41 +0700

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,114 @@
 function Home() {
-    return <h1>Home pages</h1>;
+    return ( <h1>Home pages</h1>
+        <div class="text">
+			<p>
+	Dịch Vụ Xây Kênh Youtube Chuyên Nghiệp | Dịch Vụ Sản Xuất Video Youtube | Đơn vị Xây Kênh Youtube Uy Tín</p>
+<blockquote>
+	<h2 data-end="274" data-start="202">
+		<span style="font-size: 16px;">Bạn muốn phát triển&nbsp;</span><strong data-end="326" data-start="296" style="font-size: 16px;">kênh YouTube chuyên nghiệp</strong><span style="font-size: 16px;">, xây dựng hình ảnh cá nhân, doanh nghiệp hoặc tạo ra kênh truyền thông hiệu quả? THT Media cung cấp&nbsp;</span><strong data-end="455" data-start="427" style="font-size: 16px;">dịch vụ YouTube trọn gói</strong><span style="font-size: 16px;">&nbsp;từ ý tưởng đến sản xuất và tăng trưởng kênh.</span></h2>
+</blockquote>
+<hr data-end="505" data-start="502">
+<h3 data-end="560" data-start="507">
+	<strong data-end="560" data-start="514">Các dịch vụ YouTube nổi bật tại THT Media:</strong></h3>
+<ul data-end="1055" data-start="562">
+	<li data-end="612" data-start="562">
+		<p data-end="612" data-start="564">
+			<strong data-end="610" data-start="564">Xây dựng và phát triển kênh YouTube từ A–Z</strong></p>
+	</li>
+	<li data-end="707" data-start="613">
+		<p data-end="707" data-start="615">
+			<strong data-end="655" data-start="615">Sản xuất video YouTube chuyên nghiệp</strong>: TVC, review, vlog, phỏng vấn, hoạt hình, shorts…</p>
+	</li>
+	<li data-end="779" data-start="708">
+		<p data-end="779" data-start="710">
+			<strong data-end="777" data-start="710">Lên ý tưởng nội dung, viết kịch bản theo hướng viral, bắt trend</strong></p>
+	</li>
+	<li data-end="849" data-start="780">
+		<p data-end="849" data-start="782">
+			<strong data-end="814" data-start="782">Dựng video YouTube chuẩn SEO</strong>, chèn hiệu ứng, phụ đề, nhạc nền</p>
+	</li>
+	<li data-end="894" data-start="850">
+		<p data-end="894" data-start="852">
+			<strong data-end="892" data-start="852">Thiết kế ảnh thumbnail thu hút click</strong></p>
+	</li>
+	<li data-end="966" data-start="895">
+		<p data-end="966" data-start="897">
+			<strong data-end="919" data-start="897">Tối ưu SEO YouTube</strong>, nghiên cứu từ khóa, mô tả, thẻ tag, tiêu đề</p>
+	</li>
+	<li data-end="1055" data-start="967">
+		<p data-end="1055" data-start="969">
+			<strong data-end="1009" data-start="969">Quản trị và tăng trưởng kênh YouTube</strong>: Lên lịch đăng, thống kê, định hướng nội dung</p>
+	</li>
+</ul>
+<hr data-end="1060" data-start="1057">
+<h3 data-end="1095" data-start="1062">
+	<strong data-end="1095" data-start="1069">THT Media dành cho ai?</strong></h3>
+<ul data-end="1338" data-start="1097">
+	<li data-end="1159" data-start="1097">
+		<p data-end="1159" data-start="1099">
+			Doanh nghiệp cần kênh YouTube giới thiệu sản phẩm, dịch vụ</p>
+	</li>
+	<li data-end="1222" data-start="1160">
+		<p data-end="1222" data-start="1162">
+			Cá nhân, KOL, chuyên gia muốn xây dựng thương hiệu cá nhân</p>
+	</li>
+	<li data-end="1291" data-start="1223">
+		<p data-end="1291" data-start="1225">
+			Chủ spa, bất động sản, giáo dục… muốn tăng khách hàng từ YouTube</p>
+	</li>
+	<li data-end="1338" data-start="1292">
+		<p data-end="1338" data-start="1294">
+			Người mới bắt đầu – chưa biết bắt đầu từ đâu</p>
+	</li>
+</ul>
+<hr data-end="1343" data-start="1340">
+<h3 data-end="1377" data-start="1345">
+	<strong data-end="1377" data-start="1351">Cam kết của THT Media:</strong></h3>
+<ul data-end="1587" data-start="1379">
+	<li data-end="1422" data-start="1379">
+		<p data-end="1422" data-start="1381">
+			Đội ngũ sáng tạo nội dung chuyên nghiệp</p>
+	</li>
+	<li data-end="1474" data-start="1423">
+		<p data-end="1474" data-start="1425">
+			Quay – dựng video chất lượng cao, đúng deadline</p>
+	</li>
+	<li data-end="1532" data-start="1475">
+		<p data-end="1532" data-start="1477">
+			Kênh phát triển bền vững, đúng hướng, đúng chiến lược</p>
+	</li>
+	<li data-end="1587" data-start="1533">
+		<p data-end="1587" data-start="1535">
+			Tư vấn và hỗ trợ 1:1 trong suốt quá trình triển khai</p>
+	</li>
+</ul>
+<hr data-end="1592" data-start="1589">
+<p data-end="1665" data-start="1594">
+	&nbsp;<strong data-end="1665" data-start="1597">Liên hệ ngay để được tư vấn xây dựng kênh YouTube chuyên nghiệp:</strong></p>
+<ul data-end="6735" data-start="6538" font-size:="" inter="" style="box-sizing: border-box; list-style-position: initial; list-style-image: initial; margin: 0px 0px 0px 2.5rem; padding: 0px; color: rgb(51, 51, 51); font-family: ">
+	<li data-end="6602" data-start="6538" style="box-sizing: border-box; margin: 0px 0px 0.5rem;">
+		<h3>
+			<span data-end="6552" data-start="6540" style="box-sizing: border-box; font-weight: var(--bold-font-weight,bold );">Địa chỉ:</span><span style="box-sizing: border-box;">&nbsp;</span>Số 263 Bình Than, Đại Phúc, Bắc Ninh.</h3>
+	</li>
+	<li data-end="6632" data-start="6603" style="box-sizing: border-box; margin: 0px 0px 0.5rem;">
+		<h3>
+			<span data-end="6617" data-start="6605" style="box-sizing: border-box; font-weight: var(--bold-font-weight,bold );">Hotline:</span><span style="box-sizing: border-box;">&nbsp;</span>0375.433.678</h3>
+	</li>
+	<li data-end="6669" data-start="6633" style="box-sizing: border-box; margin: 0px 0px 0.5rem;">
+		<h3>
+			<span data-end="6645" data-start="6635" style="box-sizing: border-box; font-weight: var(--bold-font-weight,bold );">Email:</span><span style="box-sizing: border-box;">&nbsp;</span><a data-end="6667" data-start="6646" rel="noopener" style="box-sizing: border-box; background: none; transition-property: background, box-shadow, border, color, opacity, text-decoration-color, text-underline-offset, transform; transition-duration: 0.2s; color: var(--color-content-link); outline: none !important;">thtmediaqvm@gmail.com</a></h3>
+	</li>
+	<li data-end="6735" data-start="6670" style="box-sizing: border-box; margin: 0px 0px 0.5rem;">
+		<h3>
+			<span data-end="6684" data-start="6672" style="box-sizing: border-box; font-weight: var(--bold-font-weight,bold );">Website:</span><span style="box-sizing: border-box;">&nbsp;</span><a data-end="6735" data-start="6685" href="https://thtmedia.com.vn/" rel="noopener" style="box-sizing: border-box; text-decoration-line: none; background: none; transition-property: background, box-shadow, border, color, opacity, text-decoration-color, text-underline-offset, transform; transition-duration: 0.2s; outline: none !important;" target="_new">https://thtmedia.com.vn</a></h3>
+	</li>
+	<li data-end="4955" data-start="4921" style="box-sizing: border-box; margin: 0px;">
+		<h3>
+			<span style="box-sizing: border-box; font-weight: var(--bold-font-weight,bold );">Fanpage:<span style="box-sizing: border-box;">&nbsp;</span></span><a href="https://www.facebook.com/tusonthanhpho" rel="nofollow noopener" style="box-sizing: border-box; text-decoration-line: none; background: none; transition-property: background, box-shadow, border, color, opacity, text-decoration-color, text-underline-offset, transform; transition-duration: 0.2s; outline: none !important;" target="_blank">THT Media – Sản xuất TVC, Phim giới thiệu doanh nghiệp</a></h3>
+	</li>
+</ul>
+        </div>
+        );
 }
 
 export default Home;
